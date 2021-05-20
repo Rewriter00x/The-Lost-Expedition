97d926e9eb1903c78093d1a854eeceefec9e7d01
Rewriter <colossus00x@icloud.com>
2021-05-20 18:06:45 +0300

    Added playCard method to Card.java

--- src/Card.java
+++ src/Card.java
@@ -2,13 +2,22 @@ import java.util.ArrayList;
 
 public class Card {
 
-    public Card(int number, ArrayList<ArrayList<Effect>> yellowEffects, ArrayList<ArrayList<Effect>> redEffects, ArrayList<ArrayList<Effect>> blueEffects) {
+    public Card(int number, ArrayList<Effect> yellowEffects, ArrayList<ArrayList<Effect>> redEffects, ArrayList<ArrayList<Effect>> blueEffects) {
         this.number=number;
         this.yellowEffects=yellowEffects;
         this.redEffects=redEffects;
         this.blueEffects=blueEffects;
     }
 
+    public void playCard() {
+        if (yellowEffects.size()>0) for (Effect e : yellowEffects) e.doEffect();
+        int n=0;// get number of red line
+        if (redEffects.size()>0) for (Effect e: redEffects.get(0)) e.doEffect();
+        n=0; // get number of blue line (n==-1 is no blue effect)
+        if (n!=-1 && blueEffects.size()>0) for (Effect e: blueEffects.get(0)) e.doEffect();
+    }
+
     private final int number;
-    private ArrayList<ArrayList<Effect>> yellowEffects,redEffects,blueEffects;
+    private final ArrayList<Effect> yellowEffects;
+    private final ArrayList<ArrayList<Effect>> redEffects,blueEffects;
 }
