c5d0cd17fc5c955edc0f29d2afbb517618a3e4b9
Rewriter <colossus00x@icloud.com>
2021-05-20 17:03:52 +0300

    Added effect lists to Card.java

--- src/Card.java
+++ src/Card.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+
 public class Card {
 
-    public Card(int number) {
+    public Card(int number, ArrayList<ArrayList<Effect>> yellowEffects, ArrayList<ArrayList<Effect>> redEffects, ArrayList<ArrayList<Effect>> blueEffects) {
         this.number=number;
+        this.yellowEffects=yellowEffects;
+        this.redEffects=redEffects;
+        this.blueEffects=blueEffects;
     }
 
     private final int number;
+    private ArrayList<ArrayList<Effect>> yellowEffects,redEffects,blueEffects;
 }
