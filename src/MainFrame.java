745ba912e44880bfef47512fbc465641d680aa56
Khoroshun-Oleksandra <oleksandra.khoroshun@ukma.edu.ua>
2021-05-23 17:37:46 +0200

    added MainFrame

---
+++ src/MainFrame.java
@@ -0,0 +1,106 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.BorderFactory;
+
+
+public class MainFrame extends JFrame{
+    public MainFrame(){
+        super();
+        this.setSize(WIDTH, HEIGHT);
+        try {
+            BufferedImage img = ImageIO.read(new File("background.png"));
+            this.setContentPane(new JLabel(new ImageIcon(img)));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            BufferedImage img = ImageIO.read(new File("glass.png"));
+            this.setIconImage(img);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        this.getContentPane().setLayout(null);
+
+        SINGLE.setBounds(WIDTH/2-WIDTH/4/2,HEIGHT/2-2*(HEIGHT/8)-20,WIDTH/4,HEIGHT/8);
+        SINGLE.setText("SINGLEPLAYER");
+        SINGLE.setFont(new Font("OCR A Extended", Font.PLAIN, 15));
+        SINGLE.setBackground(new Color(130, 204, 217));
+        SINGLE.setBorder(BorderFactory.createLineBorder(new Color(6, 63, 94)));
+        this.add(SINGLE);
+
+        MULTI.setBounds(WIDTH/2-WIDTH/4/2,HEIGHT/2-(HEIGHT/8),WIDTH/4,HEIGHT/8);
+        MULTI.setText("MULTIPLAYER");
+        MULTI.setFont(new Font("OCR A Extended", Font.PLAIN, 15));
+        MULTI.setBackground(new Color(130, 204, 217));
+        MULTI.setBorder(BorderFactory.createLineBorder(new Color(6, 63, 94)));
+        this.add(MULTI);
+
+        HELP.setBounds(WIDTH/2-WIDTH/4/2,HEIGHT/2+20,WIDTH/4,HEIGHT/8);
+        HELP.setText("HELP");
+        HELP.setFont(new Font("OCR A Extended", Font.PLAIN, 15));
+        HELP.setBackground(new Color(130, 204, 217));
+        HELP.setBorder(BorderFactory.createLineBorder(new Color(6, 63, 94)));
+        this.add(HELP);
+
+        name.setBounds(WIDTH/2-WIDTH/4+10,20,WIDTH/2-20,HEIGHT/11);
+        name.setText("THE LOST EXPEDITION");
+        name.setOpaque(true);
+        name.setBackground(new Color(3, 87, 30));
+        name.setForeground(new Color(245, 205, 76));
+        name.setFont(new Font("OCR A Extended", Font.PLAIN, 30));
+        this.add(name);
+
+        authors.setBounds(WIDTH/2-WIDTH/4,HEIGHT-60,WIDTH/2,HEIGHT/18);
+        authors.setText("by Burliai Danylo and Khoroshun Oleksandra");
+       authors.setOpaque(true);
+        authors.setBackground(new Color(3, 87, 30));
+        authors.setForeground(new Color(245, 205, 76));
+        authors.setFont(new Font("OCR A Extended", Font.PLAIN, 15));
+        this.add(authors);
+
+        this.setTitle("The Lost Expedition");
+
+        SINGLE.addActionListener( new ActionListener(){
+            public void actionPerformed(ActionEvent ae) {
+                if(ae.getSource()==SINGLE) {
+
+                }
+            }
+        });
+        MULTI.addActionListener( new ActionListener(){
+            public void actionPerformed(ActionEvent ae) {
+                if(ae.getSource()==MULTI) {
+
+                }
+            }
+        });
+        HELP.addActionListener( new ActionListener(){
+            public void actionPerformed(ActionEvent ae) {
+                if(ae.getSource()==HELP) {
+                    if (Desktop.isDesktopSupported()) {
+                        try {
+                            File myFile = new File( "rules.pdf");
+                            Desktop.getDesktop().open(myFile);
+                        } catch (IOException ex) {
+
+                        }
+                    }
+                }
+            }
+        });
+    }
+    protected JLabel name=new javax.swing.JLabel();
+    protected JLabel authors=new javax.swing.JLabel();
+    protected JButton SINGLE=new javax.swing.JButton();
+    protected JButton MULTI=new javax.swing.JButton();
+    protected JButton HELP=new javax.swing.JButton();
+    private int WIDTH=750;
+    private int HEIGHT=450;
+}
--- src/TheLostExpedition.java
+++ src/TheLostExpedition.java
@@ -1,8 +1,13 @@
+import java.awt.*;
 import java.util.*;
 
 public class TheLostExpedition {
     public static void main(String[] args) {
         fillCards();
+        MainFrame frame = new MainFrame();
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
+        frame.setVisible(true);
     }
 
     private static void fillCards() {
