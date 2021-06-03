c5114df3c8493a8dcfcc939a4387434543bc6a42
Rewriter <colossus00x@icloud.com>
2021-06-03 12:46:31 +0300

    WinDialog

---
+++ src/WinDialog.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class WinDialog extends JDialog {
+    public WinDialog(Frame frame, boolean modal, String title, String labelText) {
+        super(frame, modal);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new GridLayout(2,1));
+        setTitle(title);
+        JLabel label = new JLabel(labelText);
+        label.setFont(new Font("Arial", Font.PLAIN, 26));
+        JButton button = new JButton("OK");
+        button.setSize(200,100);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {dispose();}
+        });
+        add(label);
+        add(button);
+        pack();
+        setLocation((Toolkit.getDefaultToolkit().getScreenSize().width-getSize().width)/2,(Toolkit.getDefaultToolkit().getScreenSize().height-getSize().height)/2);
+    }
+}
